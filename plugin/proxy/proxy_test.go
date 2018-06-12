269f61e461971541ec78d1b574e8f4f44a82aded
Eugen Kleiner <ekleiner@infoblox.com>
2018-06-12 17:42:55 +0300

    plugin/proxy: Mark upstream broken if i/o timeout - add test

--- plugin/proxy/proxy_test.go
+++ plugin/proxy/proxy_test.go
@@ -1,7 +1,9 @@
 package proxy
 
 import (
+	"context"
 	"fmt"
+	"net"
 	"net/http"
 	"net/http/httptest"
 	"strings"
@@ -9,7 +11,10 @@ import (
 	"testing"
 	"time"
 
+	"github.com/coredns/coredns/plugin/pkg/dnstest"
+	"github.com/coredns/coredns/plugin/test"
 	"github.com/mholt/caddy/caddyfile"
+	"github.com/miekg/dns"
 )
 
 func TestStop(t *testing.T) {
@@ -68,5 +73,48 @@ func TestStop(t *testing.T) {
 				t.Errorf("Test %d, expected no more healthchecks after shutdown. got: %d healthchecks after shutdown", i, counterAfterWaiting-counterAfterShutdown)
 			}
 		})
+	}
+}
+
+func TestProxySequentialPolicy(t *testing.T) {
+	// Set up valid DNS server.
+	s := dnstest.NewServer(func(w dns.ResponseWriter, r *dns.Msg) {
+		ret := new(dns.Msg)
+		ret.SetReply(r)
+		ret.Answer = append(ret.Answer, test.A("example.org. IN A 127.0.0.1"))
+		w.WriteMsg(ret)
+	})
+	defer s.Close()
+
+	// Set up port which will not respond. Emulates unreachable server (i/o timeout)
+	u, _ := net.Listen("tcp", ":0")
+	defer u.Close()
+
+	// Set up proxy with 2 endpoints: 1st not responding, 2nd valid.
+	// "force_tcp" is for simplification of unreachable server setup
+	proxyStanza := fmt.Sprintf("proxy . %v %v {\n policy sequential\n protocol dns force_tcp\n}",
+		u.Addr().String(), s.Addr)
+	c := caddyfile.NewDispenser("Testfile", strings.NewReader(proxyStanza))
+	upstreams, err := NewStaticUpstreams(&c)
+	if err != nil {
+		t.Errorf("Expected no error. Got: %s", err)
+	}
+	proxy := Proxy{Upstreams: &upstreams}
+
+	// Call ServeDNS and make sure that proxy successfully switched to valid 2nd endpoind and got expected respponce
+	req := new(dns.Msg)
+	req.SetQuestion("example.org.", dns.TypeA)
+	rrw := dnstest.NewRecorder(&test.ResponseWriter{})
+	if _, err := proxy.ServeDNS(context.TODO(), rrw, req); err != nil {
+		t.Fatalf("Expected no error. Error: %v", err.Error())
+	}
+	if len(rrw.Msg.Answer) != 1 {
+		t.Fatalf("Expected exactly one RR in the answer section, got: %d", len(rrw.Msg.Answer))
+	}
+	if rrw.Msg.Answer[0].Header().Rrtype != dns.TypeA {
+		t.Errorf("Expected RR to A, got: %d", rrw.Msg.Answer[0].Header().Rrtype)
+	}
+	if rrw.Msg.Answer[0].(*dns.A).A.String() != "127.0.0.1" {
+		t.Errorf("Expected 127.0.0.1, got: %s", rrw.Msg.Answer[0].(*dns.A).A.String())
 	}
 }
